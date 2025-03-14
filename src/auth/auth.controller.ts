c0f7ca7a1dce8206e7ca37573923ab83d1aa8d3c
Edgar Alan Rodriguez Amador <a.rodriguez@industrial-code.com>
2025-03-14 00:09:04 -0600

    feat: register store page

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -5,7 +5,7 @@ import { SignupDto, SigninDto } from './dto/auth.dto';
 @Controller('api/v1/auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
-
+  
   @Post('signup')
   async signup(@Body(ValidationPipe) data: SignupDto) {
     return this.authService.signup(data);
