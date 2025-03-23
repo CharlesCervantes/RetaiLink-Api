4cd89ebc20209325d237ebafaea6b3dce66ff784
Edgar Alan Rodriguez Amador <a.rodriguez@industrial-code.com>
2025-03-23 17:45:39 -0600

    feat: registro de usuario admin

--- src/master/establecimientos/dto/create-establecimiento.dto.ts
+++ src/master/establecimientos/dto/create-establecimiento.dto.ts
@@ -1,1 +1,15 @@
-export class CreateEstablecimientoDto {}
+import { IsString, IsPhoneNumber } from 'class-validator';
+
+export class CreateEstablecimientoDto {
+    @IsString()
+    vc_nombre: string;
+
+    @IsString()
+    vc_numero_serie: string;
+
+    @IsPhoneNumber('MX')
+    vc_telfono: string;
+
+    direccion: {}
+
+}
