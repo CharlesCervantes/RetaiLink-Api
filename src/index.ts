4cd1503b67a538924d89b9d722a1bdeae7fff4fb
Edgar Alan Rodriguez Amador <a.rodriguez@industrial-code.com>
2025-04-19 16:48:29 -0600

    New api

---
+++ src/index.ts
@@ -0,0 +1,39 @@
+import express, {Express} from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import helmet from 'helmet';
+import {testConnection} from './config/database';
+
+dotenv.config();
+
+const app: Express = express();
+const PORT = process.env.PORT || 3000;
+
+// Middlewares
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+
+// Routes
+
+
+// Ruta de prueba
+app.get('/', (req, res) => {
+    res.send('API is running...');
+});
+
+// Start server
+const startServer = async () => {
+    try {
+        await testConnection(); // Test database connection
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    } catch (error) {
+        console.error('Error starting server:', error);
+    }
+};
+
+startServer();
