6dad9aca6e623d7024d1b33a9a8be28d8d2bb742
Edgar Alan Rodriguez Amador <a.rodriguez@industrial-code.com>
2025-06-29 22:45:40 -0600

    se agregan mas ados al el usuario promotor

--- src/core/promotor.ts
+++ src/core/promotor.ts
@@ -48,7 +48,7 @@ export const create_promotor = async (promotor: Promotor, connection: PoolConnec
 export const verify_promotor = async (username: string, password: string): Promise<Promotor | null> => {
     try {
         const [rows] = await pool.query<any[]>(
-            'SELECT id_promotor, vc_username, vc_password FROM promotores WHERE vc_username = ? LIMIT 1',
+            'SELECT id_promotor, vc_username, vc_password, vc_nombre, dt_fecha_nacimiento, dt_registro FROM promotores WHERE vc_username = ? LIMIT 1',
             [username]
         );
 
