ce61698b1d6f7957b309d95c3d280e4706c42882
Edgar Rodriguez <edgaralanra@gmail.com>
2025-07-06 10:59:14 -0600

    se agrega el endpoind de retiro

--- src/app_mobile/index.ts
+++ src/app_mobile/index.ts
@@ -3,7 +3,7 @@ import express, {Router} from 'express';
 import { authMiddleware } from '../core/middleware/auth.middleware';
 
 import { crear_promotor, login_promotor } from './promotor.mobile.controller';
-import { obtener_cuenta_promotor, realizar_deposito_promotor } from './cuenta_promotor.mobile.controller';
+import { obtener_cuenta_promotor, realizar_deposito_promotor, realizar_retiro_promotor } from './cuenta_promotor.mobile.controller';
 
 const promotorRouter: Router = express.Router();
 
@@ -14,6 +14,7 @@ promotorRouter.post('/login', (req, res) => {login_promotor(req, res)});
 // Cuenta del promotor 🤑💵
 promotorRouter.post('/cuenta', authMiddleware, (req, res) => {obtener_cuenta_promotor(req, res)});
 promotorRouter.post('/cuenta/deposito', authMiddleware, (req, res) => {realizar_deposito_promotor(req, res)});
+promotorRouter.post('/cuenta/retiro', authMiddleware, (req, res) => {realizar_retiro_promotor(req, res)});
 
 
 export default promotorRouter;
