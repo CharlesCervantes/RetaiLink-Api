f90792d638c12a58b37e11af218ec2004c39562b
Edgar Alan Rodriguez Amador <a.rodriguez@industrial-code.com>
2025-08-20 23:02:56 -0600

    Endpoint para registro de usuarios negocio

--- src/app_admin/index.ts
+++ src/app_admin/index.ts
@@ -5,7 +5,7 @@ import {
     obtener_productos,
 } from './productos.admin.controller'
 import { crear_pedido_plantilla,obtener_pedido_plantilla,obtener_pedido_plantilla_por_id,eliminar_pedido_plantilla } from './pedido_plantilla.controller';
-import { login_usuario, obtener_usuarios_negocio } from './usuarios.admin.controller';
+import { login_usuario, obtener_usuarios_negocio, registrar_usuario_negocio } from './usuarios.admin.controller';
 import {crear_establecimiento, obtener_establecimiento, obtener_todos_establecimientos, actualizar_establecimiento, eliminar_establecimiento} from './establecimientos.admin.controllers';
 
 import { authMiddleware } from '../core/middleware/auth.middleware';
@@ -16,6 +16,7 @@ const adminRouter: Router = express.Router();
 // Usuarios 👤
 adminRouter.post('/login-user', (req, res) => {login_usuario(req, res)});
 adminRouter.post('/get-users-by-business', authMiddleware, (req, res) => {obtener_usuarios_negocio(req, res)});
+adminRouter.post('/register-user-by-buisness', authMiddleware, (req, res) => {registrar_usuario_negocio(req, res)});
 
 // Productos 🛒
 adminRouter.post('/create-product', authMiddleware, (req, res) => {crear_producto(req, res)});
--- src/app_admin/usuarios.admin.controller.ts
+++ src/app_admin/usuarios.admin.controller.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from 'express';
 import { compare_password, generate_token, TokenPayload } from '../core/utils';
-import { get_user_by_username, get_all_users_by_buisness } from '../core/usuarios';
+import { get_user_by_username, get_all_users_by_buisness, get_user, create_user } from '../core/usuarios';
+import pool from '../config/database';
+import { User } from '@/core/interfaces';
 
 export const login_usuario = async (req: Request, res: Response) => {
     try {
@@ -81,3 +83,67 @@ export const obtener_usuarios_negocio = async(req: Request, res: Response) =>{
         });
     }
 }
+
+export const registrar_usuario_negocio = async (req: Request, res: Response) => {
+    const connection_db = await pool.getConnection();
+    await connection_db.beginTransaction(); 
+
+    try {
+        const { vc_username, vc_password, vc_nombre, id_negocio, i_rol } = req.body;
+
+        if (!vc_username || !vc_password || !vc_nombre) {
+            throw new Error('Username, password, and name are required');
+        }
+
+        const existingUser = await get_user(vc_username);
+        if (existingUser) {
+            throw new Error('User already exists');
+        }
+
+        const newUser: User = {
+            vc_username,
+            vc_password,
+            vc_nombre,
+            id_negocio,
+            i_rol: 2 // aqui simpre se crearen usuarios admin, no superadsmin
+        };
+
+        const userId = await create_user(newUser, connection_db);
+
+        if (!userId) {
+            throw new Error('Failed to create user');
+        }
+
+
+        await connection_db.commit();
+
+        const user: User = {
+            id_usuario: userId,
+            vc_username,
+            vc_nombre,
+            vc_password: '', // No devolver la contraseña en la respuesta
+            b_activo: true,
+            dt_registro: Math.floor(Date.now() / 1000),
+            dt_actualizacion: Math.floor(Date.now() / 1000),
+            id_negocio,
+            i_rol,
+        };
+
+        return res.status(201).json({
+            ok: true,
+            data: user,
+            message: 'User created successfully',
+        });
+
+    } catch (error) {
+        await connection_db.rollback(); // 🔴 Deshacer la transacción en caso de error
+        console.error('Error creating user:', error);
+        return res.status(500).json({
+            ok: false,
+            data: null,
+            message: error instanceof Error ? error.message : 'Internal error',
+        });
+    } finally {
+        connection_db.release(); // 🔸 Liberar la conexión SIEMPRE
+    }
+};
